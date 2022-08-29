5f6d2b86160b23fa539f9937ee1f26c8d1a3ff98
Mustafa <mustafa_sahinci@hotmail.com>
2022-08-29 12:59:23 +0000

    started with the readme

---
+++ blog/tests_views.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Category, Comment
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='testuser', password='12345'
+            )
+        self.post = Post.objects.create(title='Test', author=test_user)
+        self.comment = Comment.objects.create(
+            body='Test Comment', post=self.post
+            )
+        self.category = Category.objects.create(name='Test Category')
+        self.client.login(username="testuser", password="12345")
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_get_post_create_page(self):
+        response = self.client.get('/post_create/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post_create.html')
+
+    # def test_get_post_edit_page(self):
+    #     response = self.client.get('/post_edit/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'post_edit.html')
+
+    # def test_get_post_delete_page(self):
+    #     response = self.client.get('/post_delete/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'post_delete.html')
+
+    # def test_get_categories_page(self):
+    #     response = self.client.get('/categories/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'categories.html')
+
+    # def test_get_post_detail_page(self):
+    #     response = self.client.get('/post_detail/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_post_list_page(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog.html')
