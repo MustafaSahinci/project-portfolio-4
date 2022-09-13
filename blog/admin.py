e95234e60d78601e03178360eb5893edf17ceb77
Mustafa <mustafa_sahinci@hotmail.com>
2022-09-13 16:28:22 +0000

    add comment delete function

--- blog/admin.py
+++ blog/admin.py
@@ -18,10 +18,6 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on',)
     search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
--- blog/views.py
+++ blog/views.py
@@ -165,7 +165,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("created_on")
+        comments = post.comments.order_by("created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -186,7 +186,7 @@ class PostDetail(View):
 
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("created_on")
+        comments = post.comments.order_by("created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -233,6 +233,19 @@ class CommentDelete(DeleteView):
     def test_func(self):
         comment = self.get_object()
         return comment.name == self.request.user.username
+
+    def get_success_url(self):
+        post = self.object.post
+        return reverse_lazy('post_detail', kwargs={'slug': post.slug})
+
+
+class CommentUpdate(UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
         post = self.object.post
